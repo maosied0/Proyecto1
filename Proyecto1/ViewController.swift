1c5639c7cbc29bcb58ee7af0a88c331acbd0b44e
jonathanibarra <maosied@gmail.com>
2026-02-13 11:15:36 +0100

    Initial Commit

---
+++ Proyecto1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto1
+//
+//  Created by Jonathan Ibarra on 13/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
